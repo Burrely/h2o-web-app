361771a9e6a07f90de918a6d4f5d80c37fa9c05d
Unknown <james.kessels@student.fontys.nl>
2020-05-22 15:44:07 +0200

    Filter buttons filter out notifications based on which button you clicked on

--- Assets/Scripts/filter.js
+++ Assets/Scripts/filter.js
@@ -5,7 +5,21 @@ function switchFilter(id) {
     }
 
     document.getElementById(id).className += " active";
-    
+
+    notifications = document.getElementsByClassName("box");
+    for (j = 0; j < notifications.length; j++) {
+        if(id != "alles") {
+            if (!notifications[j].className.includes(id)) {
+                notifications[j].style.display = "none";
+            }
+            else {
+                notifications[j].style.display = "";
+            }
+        }
+        else {
+            notifications[j].style.display = "";
+        }
+    }
 }
 
 window.addEventListener("load", () => {
