05a5802c9c26619cdf929af98070334a089b39f2
Burrely <dinosammy1@live.nl>
2020-05-22 15:14:39 +0200

    Waiting for page load implementation on javascript
    Something something Javascript, hoi James.

--- Assets/Scripts/filter.js
+++ Assets/Scripts/filter.js
@@ -1,18 +1,23 @@
-elements = document.getElementsByClassName("filterBtn")
+function switchFilter(id) {
 
-for(i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", switchFilter(this.id));
-}
-
-document.getElementById("alles").addEventListener("click", switchFilter(this.id));
-document.getElementById("mijlpalen").addEventListener("click", switchFilter(this.id));
-document.getElementById("tips").addEventListener("click", switchFilter(this.id));
-document.getElementById("alles").addEventListener("click", switchFilter(this.id));
-
-function switchFilter(id) { 
     for(i = 0; i < elements.length; i++) {
         elements[i].className = "filterBtn"
     }
 
     document.getElementById(id).className += " active";
+    
 }
+
+window.addEventListener("load", () => {
+    
+    elements = document.getElementsByClassName("filterBtn");
+
+    for(i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("click", switchFilter(this.id));
+    }
+
+    document.getElementById("alles").addEventListener("click", switchFilter(this.id));
+    document.getElementById("mijlpalen").addEventListener("click", switchFilter(this.id));
+    document.getElementById("tips").addEventListener("click", switchFilter(this.id));
+    document.getElementById("alles").addEventListener("click", switchFilter(this.id));
+})
